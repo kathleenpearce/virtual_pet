d793c6d5bcba3fb66a2dd2d07e1134f9b470c863
Kathleen pearce <kathleenpearce@live.com>
2019-05-27 19:27:54 +0000

    skeleton done

--- src/client/Feed.jsx
+++ src/client/Feed.jsx
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
+  import React, { Component } from 'react';
 
-import './Home.css'
+  import './Home.css'
 
 
 
-export default class SelectMate extends Component {
-  render () {
-    return (
-      <div>
-        <div className="feeding-page-content">
-        <div className="feeding-container">
-        <a href="http://example.com"><div className="food-1">Bone<i class="fas fa-bone"></i></div><br />
-        <div className="food-price-1">$10</div></a>
-        <a href="http://example.com"><div className="food-2">Food Option 2</div><br />
-        <div className="food-price-2">$25</div></a>
-        <a href="http://example.com"><div className="food-3">Food Option 3</div><br />
-        <div className="food-price-3">$45</div></a>
-        <a href="#" className="button">Feed</a>
+  export default class SelectMate extends Component {
+    render () {
+      return (
+        <div>
+          <div className="feeding-page-content">
+          <div className="feeding-container">
+          <a href="http://example.com"><div className="food-1">Bone<i class="fas fa-bone"></i></div><br />
+          <div className="food-price-1">$10</div></a>
+          <a href="http://example.com"><div className="food-2">Food Option 2</div><br />
+          <div className="food-price-2">$25</div></a>
+          <a href="http://example.com"><div className="food-3">Food Option 3</div><br />
+          <div className="food-price-3">$45</div></a>
+          <a href="#" className="button">Feed</a>
+          </div>
         </div>
       </div>
-    </div>
-    )
+      )
+    }
   }
-}
--- src/client/PetProfile.js
+++ src/client/PetProfile.js
@@ -1,54 +1,54 @@
-import React, { Component } from 'react';
+  import React, { Component } from 'react';
 
-import './Home.css'
-import CreatureCard from './CreatureCard'
+  import './Home.css'
+  import CreatureCard from './CreatureCard'
 
-export default class PetProfile extends Component {
-  render () {
-    return (
+  export default class PetProfile extends Component {
+    render () {
+      return (
 
-<div>
-<br /><br />
-<div className="pet-profile-content">
-<div className="pet-profile-creature-card">
-<div className='creature-grid-item'><CreatureCard /></div>
-</div>
+    <div>
+    <br /><br />
+    <div className="pet-profile-content">
+    <div className="pet-profile-creature-card">
+    <div className='creature-grid-item'><CreatureCard /></div>
+    </div>
 
-<div className="pet-profile-info">
-<div className="pet-profile-name"><h2>Pet Name</h2></div>
+    <div className="pet-profile-info">
+    <div className="pet-profile-name"><h2>Pet Name</h2></div>
 
-            <div class="pet-profile-stats">
-            <ul> Energy:  </ul><br />
-            <ul> Strength: </ul><br />
-            <ul>Hunger: </ul><br />
-            <ul> Intelligence: </ul><br />
-            <ul>Happiness: </ul><br />
-            <ul> Gender:</ul><br />
+                <div class="pet-profile-stats">
+                <ul> Energy:  </ul><br />
+                <ul> Strength: </ul><br />
+                <ul>Hunger: </ul><br />
+                <ul> Intelligence: </ul><br />
+                <ul>Happiness: </ul><br />
+                <ul> Gender:</ul><br />
 
-        </div>
-
-
-  <div className="pet-profile-buttons">
-  <a href="#" className="button" >Send To Work</a>
-  <a href="#" className="button">Feed</a> <a href="#" className="button">Breed</a>
-  <a href="#" className="button-free">Set Free</a>
-
- </div>
-
-</div>
-
-</div>
+            </div>
 
 
+      <div className="pet-profile-buttons">
+      <a href="#" className="button" >Send To Work</a>
+      <a href="#" className="button">Feed</a> <a href="#" className="button">Breed</a>
+      <a href="#" className="button-free">Set Free</a>
 
-</div>
+     </div>
+
+    </div>
+
+    </div>
 
 
+
+    </div>
 
 
 
 
 
-      )
+
+
+        )
+    }
   }
-}
